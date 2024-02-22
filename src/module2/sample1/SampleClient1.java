61f85d3d8c085bff5cf126deb774d4fe67227497
Llena Anthony <2235774@slu.edu.ph>
2024-02-22 20:46:56 +0800

    + Create a server program that asks for the name of the connecting client and his age. The server sends a response to the client if he/she is qualified to vote in the upcoming national elections or not.

---
+++ src/module2/sample1/SampleClient1.java
@@ -0,0 +1,42 @@
+package module2.sample1;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.net.Socket;
+import java.util.Scanner;
+public class SampleClient1 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int port = 2000;
+
+        try (
+                Socket socket = new Socket("localhost", 2000);
+                BufferedReader streamRdr = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream()));
+                PrintWriter streamWtr = new PrintWriter(
+                        socket.getOutputStream(), true);
+            ) {
+            // read and print message from server
+            System.out.println(streamRdr.readLine()); // 1
+            String input = keyboard.nextLine();
+
+            // send input to the server
+            streamWtr.println(input); // 2
+
+            // read and print message from the server
+            System.out.println(streamRdr.readLine());
+            input = keyboard.nextLine();
+
+            streamWtr.println(input);
+
+            System.out.println(streamRdr.readLine());
+            System.out.println(streamRdr.readLine());
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/module2/sample1/SampleServer1.java
@@ -0,0 +1,60 @@
+package module2.sample1;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.net.ServerSocket;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.Buffer;
+
+public class SampleServer1 {
+    public static void main(String[] args) {
+        int port = 2000;
+        try (
+                ServerSocket serverSocket = new ServerSocket(port);
+                Socket clientSocket = serverSocket.accept();
+
+                BufferedReader streamRdr = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+
+                PrintWriter streamWtr = new PrintWriter(
+                        clientSocket.getOutputStream(), true);
+                ){
+
+            // server send
+            streamWtr.println("What is your name?  "); // 1
+
+            // server accepts input from client
+            String name = streamRdr.readLine(); // 2
+             int age;
+            while (true) {
+                streamWtr.println("What is your age? ");
+                try {
+                    age = Integer.parseInt(streamRdr.readLine());
+                    if (age <= 0) {
+                        throw new NumberFormatException();
+                    } else {
+                        break;
+                    }
+                }  catch (NumberFormatException nfe) {
+                    // print method will auto flush
+                    streamWtr.println("Please enter a valid age.");
+                    continue;
+                }
+            }
+
+                if(age >= 18){
+                    streamWtr.print(name + ", you may now exercise your right to vote!");
+                } else {
+                    streamWtr.println(name + ", you are still young to vote! ");
+                }
+                streamWtr.println("Thank you and good day! ");
+
+            }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
