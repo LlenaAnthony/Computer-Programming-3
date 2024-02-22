3eee45ea0815cfb0b7f442efa524a1eb6e33a070
Llena Anthony <2235774@slu.edu.ph>
2024-02-22 21:04:07 +0800

    + fixed format in main...

--- src/module3/SampleRemote.java
+++
@@ -1,8 +0,0 @@
-package module3;
-
-import java.rmi.RemoteException;
-import java.rmi.Remote;
-
-public interface SampleRemote extends Remote{
-    int countCharacters (String value) throws RemoteException;
-}
